7fb2f0ef25b0d79682994502ece83cf776fb58c3
foodrubiks <kalvin.khuu@gmail.com>
2020-04-18 10:56:24 -0400

    Added first version of ice cream cone

---
+++ Nikrykal/Assets/Scripts/IceCreamCone.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCreamCone : MonoBehaviour
+{
+    public GameObject[] Balls;
+
+    private float TimeElapsed;
+
+    private GameObject CurrentBall;
+    private int CurrentBallIndex;
+
+    public float TimePerScoopDeath = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeElapsed = 0.0f;
+        CurrentBallIndex = 0;
+        CurrentBall = Balls[CurrentBallIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       TimeElapsed += Time.deltaTime;
+       Debug.Log("Time Elapsed = " + TimeElapsed.ToString());
+          
+        if (TimeElapsed >= TimePerScoopDeath)
+        {
+            CurrentBall.SetActive(false);
+            TimeElapsed = 0.0f;
+            if (CurrentBallIndex < Balls.Length)
+            {
+                CurrentBallIndex++;
+                CurrentBall = Balls[CurrentBallIndex];
+            }
+        }
+               
+
+    }
+}
