a11268c6b8e6b0b8256291bcff59f10a539069f5
npw22 <nickwilby@gmx.com>
2020-04-18 18:20:33 -0400

    Added first version of the child

---
+++ Nikrykal/Assets/Child.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Child : MonoBehaviour
+{
+    private int Multiplier = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetMultiplier()
+    {
+        return Multiplier;
+
+    }
+
+    public void SetMultiplier(int NewMultiplier)
+    {
+        Multiplier = NewMultiplier;
+
+    }
+}
