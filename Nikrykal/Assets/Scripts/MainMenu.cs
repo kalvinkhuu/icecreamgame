d189f9931d1c434a875cbbe9eb2d163f21b4b4d3
foodrubiks <kalvin.khuu@gmail.com>
2020-04-19 17:12:53 -0400

    Select menu 2.0

--- Nikrykal/Assets/Scripts/MainMenu.cs
+++ Nikrykal/Assets/Scripts/MainMenu.cs
@@ -18,6 +18,14 @@ public class MainMenu : MonoBehaviour
 
     }
 
+    public void PlayGame()
+
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
 
 
     public void QuitGame()
