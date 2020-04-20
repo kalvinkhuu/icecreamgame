8a240db3a31efeb79e7555eb0f742df403f9f681
npw22 <nickwilby@gmx.com>
2020-04-20 00:08:59 -0400

    Main Menu navigable by gamepad

--- Nikrykal/Assets/Scripts/MainMenu.cs
+++ Nikrykal/Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 
@@ -9,6 +10,54 @@ public class MainMenu : MonoBehaviour
 {
     public static int ChosenNumPlayers = 4;
 
+    Button PlayButton;
+    Button CreditsButton;
+    bool PlaybuttonSelected = true;
+
+    void Awake()
+    {
+        Button [] buttons = GetComponentsInChildren<Button>();
+        PlayButton = buttons[0];
+        CreditsButton = buttons[1];
+    }
+
+    void Start()
+    {
+        PlayButton.Select();
+    }
+
+    public void BackToMainMenu()
+    {
+        PlayButton.Select();
+    }
+
+    public void BackToMainMenuFromCredits()
+    {
+        CreditsButton.Select();
+    }
+
+    void Update()
+    {
+        if (hinput.anyGamepad.leftStick.down.justPressed)
+        {
+            CreditsButton.Select();
+        }
+        if (hinput.anyGamepad.leftStick.up.justPressed)
+        {
+            PlayButton.Select();
+        }
+        if (hinput.anyGamepad.A.justPressed)
+        {
+            if (PlaybuttonSelected)
+            {
+                PlayButton.onClick.Invoke();
+            }
+            else
+            {
+                CreditsButton.onClick.Invoke();
+            }
+        }
+    }
 
     public void GoToSelectMenu()
     {
